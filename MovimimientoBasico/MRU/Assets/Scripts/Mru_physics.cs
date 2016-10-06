1e4ffbd36c4f81eba74a821658ab41e399fefd3d
Oscar Espeso Gil <oscar.espeso@live.u-tad.com>
2016-10-06 10:51:48 +0200

    Terminado script básico de movimiento del player

---
+++ MovimimientoBasico/MRU/Assets/Scripts/Mru_physics.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mru_physics : MonoBehaviour {
+
+	private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = this.GetComponent<Rigidbody>();
+		rb.velocity = new Vector3(0F, 0F, 5F);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
