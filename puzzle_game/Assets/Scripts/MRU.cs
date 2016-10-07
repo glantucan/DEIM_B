d0371d44d07246b08be543775a7da7d7a8883ee8
Oscar Espeso Gil <oscar.espeso@live.u-tad.com>
2016-10-07 14:57:59 +0200

    interruptores básicos

--- puzzle_game/Assets/Scripts/MRU.cs
+++ puzzle_game/Assets/Scripts/MRU.cs
@@ -1,24 +1,22 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Player : MonoBehaviour {
+public class MRU : MonoBehaviour {
 
 	public float speed;
 	private Vector3 direction;
-	private Rigidbody rb;
 
 	// Use this for initialization
 	void Start () {
-		this.rb = this.GetComponent<Rigidbody>();
+		
 	}
-
+	
 	// Update is called once per frame
 	void Update () {
 		float xInput = Input.GetAxisRaw("Horizontal");
 		float zInput = Input.GetAxisRaw("Vertical");
-		this.direction = xInput * Vector3.right + zInput * Vector3.forward; 
-		if (direction.magnitude != 0) {
-			this.rb.velocity = this.direction.normalized * this.speed;
-		}
+		this.direction = xInput * Vector3.right + zInput * Vector3.forward;
+		Vector3 velocity = this.speed * this.direction.normalized;
+		transform.position = transform.position + velocity * Time.deltaTime;
 	}
 }
---
+++ puzzle_game/Assets/Scripts/Player.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public float speed;
+	private Vector3 direction;
+
+	private Rigidbody rb;
+
+	private bool isNearButton; 
+
+	// Use this for initialization
+	void Start () {
+		this.rb = this.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float xInput = Input.GetAxisRaw("Horizontal");
+		float zInput = Input.GetAxisRaw("Vertical");
+		this.direction = xInput * Vector3.right + zInput * Vector3.forward;
+		if( this.direction.magnitude != 0) {
+			this.rb.velocity = this.direction.normalized * this.speed;
+		}
+
+		if (isNearButton) {
+			if ( Input.GetKeyUp(KeyCode.Space)) {
+				Debug.Log("Interruptor activado");
+			}
+		}
+	}
+
+	void OnTriggerEnter(Collider other) {
+		Debug.Log("He entrado en el trigger de un " + other.tag);
+		if(other.tag == "Switch" ) {
+			this.isNearButton = true;
+		}
+	}
+
+	void OnTriggerExit(Collider other) {
+		Debug.Log("He salido en el trigger");
+		if(other.tag == "Switch" ) {
+			this.isNearButton = false;
+		}
+	}
+}
