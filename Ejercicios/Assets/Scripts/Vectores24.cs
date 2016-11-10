75eadc23453d979d7a0914a5582bc2db824793b9
Oscar Espeso Gil <oscar.espeso@live.u-tad.com>
2016-11-10 10:55:56 +0100

    corregidos 24 y 25

---
+++ Ejercicios/Assets/Scripts/Vectores24.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Vectores24 : MonoBehaviour {
+
+	[SerializeField] private float minV;
+	[SerializeField] private float maxV;
+
+	private Vector3 direction;
+	private Vector3 velocity;
+
+	// Use this for initialization
+	void Start () {
+		direction = Random.onUnitSphere;
+		velocity = Random.Range(minV, maxV) * direction;
+		Debug.Log("Velocidad de la esfera:" + velocity.magnitude);
+		/*Vector3 dir = new Vector3 (Random.Range(-1F, 1F), 
+									Random.Range(-1F, 1F), Random.Range(-1F, 1F));
+		direction = dir.normalized;*/
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 displacement = velocity * Time.deltaTime;
+		transform.position = transform.position + displacement;
+	}
+}
---
+++ Ejercicios/Assets/Scripts/Vectores25.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Vectores25 : MonoBehaviour {
+
+	[SerializeField] private Transform sphereTr;
+	private Vector3 firstPos;
+	private float firstTime;
+	private Vector3 measuredVel;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Time.frameCount == 10) {
+			firstPos = sphereTr.position;
+			firstTime = Time.time;
+		} else if (Time.frameCount == 30) {
+			Vector3 distance = sphereTr.position - firstPos;
+			measuredVel = distance/ (Time.time - firstTime);
+		} else if (Time.frameCount == 60) {
+			Debug.Log("Velocidad medida:" + measuredVel.magnitude);
+		}
+	}
+}
