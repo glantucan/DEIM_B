aa25aaba293f15887ba3125590da6ce748174292
Oscar Espeso Gil <oscar.espeso@live.u-tad.com>
2016-12-16 14:47:22 +0100

    terminado de corregir examen 4

---
+++ examen04_alumnos/Assets/Scripts/Damage.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Damage : MonoBehaviour {
+	[SerializeField] private int damage;
+
+	private void OnCollisionEnter(Collision col) {
+		Health otherHealth = col.gameObject.GetComponent<Health>();
+
+		if (otherHealth != null) {
+			otherHealth.MakeDamage(damage);
+		}
+	}
+}
---
+++ examen04_alumnos/Assets/Scripts/Health.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Health : MonoBehaviour {
+
+	[SerializeField] private int health;
+
+	public void MakeDamage(int dam) {
+		this.health -= dam;
+		if (this.health <= 0) {
+			Debug.Log("El jugador " + this.name + " ha muerto.");
+		}
+	}
+}
